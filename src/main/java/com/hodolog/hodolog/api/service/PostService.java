e5ec6253beece5c7ea671edc2d200ab73ca2b13d
hyangkeunchoi <gidrms99@naver.com>
2022-07-26 22:40:07 +0900

    fix : 충돌 해결

--- src/main/java/com/hodolog/hodolog/api/service/PostService.java
+++ src/main/java/com/hodolog/hodolog/api/service/PostService.java
@@ -90,10 +90,4 @@ public class PostService {
         postRepository.delete(post);
     }
 
-    public void delete(Long id) {
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
-
-        postRepository.delete(post);
-    }
 }
